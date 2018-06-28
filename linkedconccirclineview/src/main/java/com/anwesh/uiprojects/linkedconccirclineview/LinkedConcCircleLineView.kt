6bd6d46975baccf596ff25e5dcd1799f99cb40ee
Anwesh43 <anweshthecool0@gmail.com>
2018-06-28 07:38:41 +0530

    adding LinkedConcCircleLineView class

--- linkedconccirclineview/src/main/java/com/anwesh/uiprojects/linkedconccirclineview/LinkedConcCircleLineView.kt
+++ linkedconccirclineview/src/main/java/com/anwesh/uiprojects/linkedconccirclineview/LinkedConcCircleLineView.kt
@@ -3,3 +3,27 @@ package com.anwesh.uiprojects.linkedconccirclineview
 /**
  * Created by anweshmishra on 28/06/18.
  */
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.view.View
+import android.view.MotionEvent
+
+class LinkedConcCircleLineView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
